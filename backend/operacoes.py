351271576b9b4ed9566f1dce4463c9e0befd7702
Alycson Moreira <alycson2@gmail.com>
2024-12-16 23:55:28 -0300

    corrigindo ramificação de branch

--- backend/operacoes.py
+++ backend/operacoes.py
@@ -10,3 +10,9 @@ class Sistema_Banco:
             return "Conta já existe."
         self.contas[numero_conta] = Conta(numero_conta)
         return "Conta criada com sucesso!"
+    
+    def consultar_saldo(self, numero_conta):
+        conta = self.contas.get(numero_conta)
+        if not conta:
+            return "Conta não encontrada."
+        return f"Saldo: R$ {conta.saldo:.2f}"
