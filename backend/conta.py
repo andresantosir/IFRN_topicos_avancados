f315e5e809c7b4768e2aa0058353707952ec6370
Musashi <andre_santos7@live.com>
2025-01-23 23:12:23 -0300

    Adicionando conta bônus e sistema de pontuação.

--- backend/conta.py
+++ backend/conta.py
@@ -5,3 +5,19 @@ class Conta:
     
     def __str__(self):
         return f"Conta : {self.numero_conta} , \nSaldo: {self.saldo}"
+    
+class ContaBonus(Conta):
+    def __init__(self, numero_conta, saldo=0):
+        super().__init__(numero_conta, saldo)
+        self.pontos = 10                    # Pontuação inicial
+
+    def depositar(self, valor):
+        resultado = super().depositar(valor)
+        if "depositados" in resultado:
+            self.pontos += valor // 100     # 1 ponto para cada R$ 100 depositados
+        return resultado
+
+    def receber_transferencia(self, valor):
+        self.pontos += valor // 200         # 1 ponto para cada R$ 200 recebidos
+        self.saldo += valor
+        return f"R$ {valor:.2f} recebidos na conta {self.numero_conta}."
--- frontend/tela.py
+++ frontend/tela.py
@@ -12,30 +12,40 @@ def main():
         print("3. Crédito")
         print("4. Débito")
         print("5. Transferência")
-        print("6. Sair")
+        print("6. Cadastrar Conta Bônus")
+        print("7. Sair")
         
         opcao = input("Escolha uma opção: ")
         
         if opcao == "1":
             numero_conta =input("Digite o número da conta: ")
             print(banco.criar_conta(numero_conta))
+            
         elif opcao == "2":
             numero_conta =input("Digite o número da conta que deseja consultar o saldo: ")
             print(banco.consultar_saldo(numero_conta))
+            
         elif opcao == "3":
             numero_conta =input("Digite o número da conta que deseja creditar valor: ")
             dinheiro = float(input("Digite a quantidade de dinheiro que deseja creditar: "))
             print(banco.creditar(numero_conta, dinheiro))
+            
         elif opcao == "4":
             numero_conta =input("Digite o número da conta que deseja retirar valor: ")
             dinheiro = float(input("Digite a quantidade de dinheiro que deseja debitar: "))
             print(banco.debitar(numero_conta, dinheiro))
+            
         elif opcao == "5":
             conta_origem = input("Digite o número da conta de origem: ")
             conta_destino = input("Digite o número da conta de destino: ")
             dinheiro = float(input("Digite o valor para transferência: "))
             print(banco.transferir(conta_origem, conta_destino, dinheiro))
+            
         elif opcao == "6":
+            numero_conta =input("Digite o número da conta: ")
+            print(banco.criar_conta_bonus(numero_conta))
+            
+        elif opcao == "7":
             print("Encerrando...")
             break
         else:
