a6001a67384ff3bc2f0b9975e741de8b231996d6
Alycson Moreira <alycson2@gmail.com>
2025-02-09 00:46:59 -0300

    Conta Simples e Conta bonus com limite de saldo negativo implementado até -1000

--- backend/conta.py
+++ backend/conta.py
@@ -1,10 +1,22 @@
 class Conta:
+
+    LIMITE_NEGATIVO = -1000.0
+
     def __init__(self, numero_conta, saldo =0.0):
         self.numero_conta = numero_conta
         self.saldo = saldo
     
     def __str__(self):
         return f"Conta : {self.numero_conta} , \nSaldo: {self.saldo}"
+    
+    def debitar(self, valor):
+       
+        # Verificação do valor para debitar do Limite da Conta
+        
+        if self.saldo - valor < Conta.LIMITE_NEGATIVO:
+            return f"Operação não permitida. O limite de saldo negativo é de R$ {Conta.LIMITE_NEGATIVO:.2f}."
+        self.saldo -= valor
+        return f"R$ {valor:.2f} debitados da conta {self.numero_conta}."
     
 class ContaBonus(Conta):
     def __init__(self, numero_conta, saldo=0):
@@ -18,6 +30,8 @@ class ContaBonus(Conta):
         return resultado
 
     def receber_transferencia(self, valor):
+        if self.saldo + valor < Conta.LIMITE_NEGATIVO:
+            return f"Operação não permitida. O limite de saldo negativo é de R$ {Conta.LIMITE_NEGATIVO:.2f}."
         self.pontos += valor // 200         # 1 ponto para cada R$ 200 recebidos
         self.saldo += valor
         return f"R$ {valor:.2f} recebidos na conta {self.numero_conta}."
