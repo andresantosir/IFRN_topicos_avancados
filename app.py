53270988dbc4866fac5c349573a559bd8dc1e4b4
Musashi <andre_santos7@live.com>
2025-02-20 20:29:26 -0300

    Adicionando CSS ao projeto e criando novas rotas comentadas para implementação futura.

--- app.py
+++ app.py
@@ -25,5 +25,37 @@ def cadastrar_conta():
     banco[numero_conta] = {"saldo": 0.0}  # Criando a conta com saldo inicial 0
     return jsonify({"mensagem": "Conta criada com sucesso!", "conta": numero_conta}), 201
 
+#@app.route('/banco/conta', methods=['GET'])
+#def consultar_conta():
+#    return 
+
+@app.route('/banco/conta/<numero_conta>/saldo', methods=['GET'])
+def consultar_saldo(numero_conta):
+    dados = request.get_json()
+    numero_conta = dados.get("numero_conta")
+
+    if not numero_conta:
+        return jsonify({"erro": "Número da conta é obrigatório"}), 400
+
+    saldo = banco.consultar_saldo(numero_conta)
+    return jsonify({"mensagem": saldo})
+
+
+#@app.route('/banco/conta/<id>/credito', methods=['PUT'])
+#def creditar():
+#   return
+
+#@app.route('/banco/conta/<id>/debito', methods=['PUT'])
+#def debitar():
+#   return
+
+#@app.route('/banco/conta/transferencia', methods=['PUT'])
+#def transferencia():
+#   return
+
+#@app.route('/banco/conta/rendimento', methods=['PUT'])
+#def render_juros():
+#   return
+
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
